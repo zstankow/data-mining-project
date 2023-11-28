bb3b0ba7fe6b947fcd6b3726bfebc6ce35ce2ea3
Zoe Zelkha <zzelkha@gmail.com>
2023-11-28 09:22:24 +0100

    Added second webpage in code

--- data_mining.py
+++ data_mining.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 NUMBER_OF_PLAYERS = 20
 
@@ -26,13 +29,54 @@ def get_players_info(driver):
     return players_info
 
 
+def get_win_loss_stats(driver):
+    win_loss_stats = driver.find_elements(By.CLASS_NAME, 'mega-table')
+
+
+    #win_loss_stats = driver.find_elements(By.CLASS_NAME, 'mega-table')
+    win_loss_info = []
+    for stat in win_loss_stats:
+        info = {'Name': stat.find_element(By.CLASS_NAME, "player-cell").text,
+                'YTD Index': stat.find_element(By.CLASS_NAME, "fifty-two-week-index-cell").text,
+                'YTD Titles': stat.find_element(By.CLASS_NAME, "fifty-two-week-titles-cell").text,
+                'YTD Win/Loss': stat.find_element(By.CLASS_NAME, "fifty-two-week-win-loss-cell").text
+                }
+
+        win_loss_info.append(info)
+    return win_loss_info
+
 def main():
     driver = webdriver.Chrome()
-    driver.get("https://www.atptour.com/en/rankings/singles")
-    for player in get_players_info(driver):
-        print(player)
-    driver.quit()
+    # driver.get("https://www.atptour.com/en/rankings/singles")
+    # for player in get_players_info(driver):
+    #     print(player)
 
+    driver.get("https://www.atptour.com/en/stats/win-loss-index")
+
+    try:
+        table = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#winLossTableList > div > table'))
+            )
+    except:
+        driver.quit()
+
+    for win_lost_stat in get_win_loss_stats(driver):
+        print(win_lost_stat)
+
+    driver.quit()
 
 if __name__ == '__main__':
     main()
+    # link = driver.find_element_by_link_text("STATS")
+    # link.click()
+    #
+    # try:
+    #     element = WebDriverWait(driver, 10).until(
+    #         EC.presence_of_element_located((By.LINK_TEXT, "Win/Loss"))
+    #     )
+    #     element.click()
+    # except None:
+    #     driver.quit()
+    #
+    # driver.quit()
+
